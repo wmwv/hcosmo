d3db74ad487985ae442e73e08e96256fb1a7758a
Michael Wood-Vasey <wmwv@pitt.edu>
2018-01-11 13:20:12 -0500

    Set up framework for table-driven testing.

--- flatlcdm_test.hs
+++ flatlcdm_test.hs
@@ -1,5 +1,20 @@
 import Test.HUnit.Approx (assertApproxEqual)
 import FlatLCDM
+import Text.Printf
 
-testAge = do
-    assertApproxEqual "FlatLCDM Age." 1e-6 8.42634602 (age 0.3 70.0 0.5)
+testAge:: Double -> Double -> Double -> Double -> Double -> IO()
+testAge om0 h0 z expected tol = do
+    assertApproxEqual (printf "FlatLCDM Age(Om0: %.2f, H0: %.2f, z: %.2f)" om0 h0 z)
+        tol expected (age om0 h0 z)
+
+testAge' :: (Double, Double, Double, Double, Double) -> IO()
+testAge' (om0, h0, z, expected, tol) = do
+    assertApproxEqual (printf "FlatLCDM Age(Om0: %.2f, H0: %.2f, z: %.2f)" om0 h0 z)
+        tol expected (age om0 h0 z)
+
+testAge'' tol (om0, h0, z, expected) = testAge om0 h0 z expected tol
+
+testAgeRunner = testAge 0.3 70.0 0.5 8.42634602 1e-6
+testAgeRunner' = mapM_ (testAge'' tol) testTableFlatLCDM
+    where tol = 1e-6
+          testTableFlatLCDM = [(0.3, 70.0, 0.5, 8.42634602)]
