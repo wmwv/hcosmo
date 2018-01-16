be304bbde09fabdde805a186ceb1288e15faa940
Michael Wood-Vasey <wmwv@pitt.edu>
2018-01-16 15:51:03 -0500

    Use value constructor directly in EdS test table specification.

--- test/Cosmo/EdSTest.hs
+++ test/Cosmo/EdSTest.hs
@@ -10,27 +10,25 @@ import Cosmo.Base
 import Cosmo.EdS
 import Text.Printf
 
-testFunction name func eds z expected tol = do
-    assertApproxEqual (printf "EdS %s(H0: %.2f, z: %.2f)" name (h0 eds) z)
-        tol expected (func h0 z)
+testFunction name func z expected tol = do
+    assertApproxEqual (printf "EdS %s (z: %.2f)" name z)
+        tol expected (func z)
 
-testFunctionZ1Z2 name func eds z1 z2 expected tol = do
-    assertApproxEqual (printf "EdS %s(H0: %.2f, z1: %.2f, z2: %2.f)" name (h0 eds) z1 z2)
-        tol expected (func eds z1 z2)
+testFunctionZ1Z2 name func z1 z2 expected tol = do
+    assertApproxEqual (printf "%s (z1: %.2f, z2: %2.f)" name z1 z2)
+        tol expected (func z1 z2)
 
-testRun tol (name, func, h, z, expected) = testFunction name func eds z expected tol
-    where eds = EdS{h0=h}
-testRunZ1Z2 tol (name, func, h, z1, z2, expected) = testFunctionZ1Z2 name func eds z1 z2 expected tol
-    where eds = EdS{h0=h}
+testRun tol (name, func, z, expected) = testFunction name func z expected tol
+testRunZ1Z2 tol (name, func, z1, z2, expected) = testFunctionZ1Z2 name func z1 z2 expected tol
 
 testTableEdS =
-    [ ("LookbackTimeEdS", lookbacktime, 70.0, 0.5, 4.24332906)
-    , ("AgeEdS", age, 70.0, 0.5, 5.06897781)
-    , ("ComovingDistanceEdS", comovingDistance, 70, 0.5, 1571.79831586)
+    [ ("LookbackTimeEdS", lookbacktime EdS{h0=70.0}, 0.5, 4.24332906)
+    , ("AgeEdS", age EdS{h0=70.0}, 0.5, 5.06897781)
+    , ("ComovingDistanceEdS", comovingDistance EdS{h0=70}, 0.5, 1571.79831586)
     ]
 
 testTableEdSZ1Z2 =
-    [ ("ComovingDistanceEdSZ1Z2", comovingDistanceZ1Z2, 70.0, 0.0, 0.5, 1571.79831586)
+    [ ("ComovingDistanceEdSZ1Z2", comovingDistanceZ1Z2 EdS{h0=70.0}, 0.0, 0.5, 1571.79831586)
     ]
 
 testRunner = mapM_ (testRun tol) testTableEdS
